19aa473698685c7efe3dbeaf2ce9a1081fb3f706
Andrew Warren <anwarren@vbi.vt.edu>
2015-03-30 19:00:34 -0400

    changing overall project name to GOGranny

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ except ImportError:
     print "You need to install networkx >= 0.99 (networkx.lanl.gov)"
     sys.exit(1)    
 
-setup(name='GOGrapher',
+setup(name='GOGranny',
             version='0.0',
-            description='A network graph representation of the Gene Ontology',
-            author='Brian Muller',
-            author_email='mullerb@musc.edu',
-            url='http://projects.dbbe.musc.edu/trac/GOGrapher',
+            description='A network graph representation of the Gene Ontology based on GOGrapher',
+            author='Andrew Warren',
+            author_email='aswarren@gmail.com',
+            url='https://github.com/aswarren/GOGranny',
             packages=['GOGrapher', 'GOGrapher.Storage', 'GOGrapher.XGraphWeighters'],
             long_description = '',
             license='GNU GPL v2',
