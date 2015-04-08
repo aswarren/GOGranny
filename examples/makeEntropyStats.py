29a5323a5153b835ab597edf266114e0fa85262e
Andrew Warren <anwarren@vbi.vt.edu>
2015-04-08 13:56:50 -0400

    add term independence column

--- examples/makeEntropyStats.py
+++ examples/makeEntropyStats.py
@@ -16,7 +16,7 @@ def main(init_args):
 	
 	onto_cat=init_args[0]
 	go_files=init_args[1:]
-	results = pd.DataFrame(columns=['date','entropy','edges','nodes','in_degree','out_degree','degree'])
+	results = pd.DataFrame(columns=['date','entropy','TI','edges','nodes','in_degree','out_degree','degree'])
 	num_file=0
 	for go_file in go_files:
 		num_file+=1
@@ -40,6 +40,9 @@ def main(init_args):
 			if k in out_degree: y=out_degree[k]
 			degree_total.append(x+y)
 		date=storage.date.split(' ')[0].replace(':','-')
+		min_uncert=-log(float(1)/cur_graph.num_nodes,2)
+		max_uncert=min_uncert*2
+		ti=(graph_uncert-min_uncert)/(max_uncert-min_uncert)#for clarity. could just be min_uncert.
 		cur_row={'date':date,'entropy':graph_uncert,'edges':cur_graph.number_of_edges(),'nodes':cur_graph.num_nodes,'in_degree':float(sum(out_degree.values()))/len(degree_total),'out_degree':float(sum(in_degree.values()))/len(degree_total),'degree':float(sum(degree_total))/len(degree_total)}
 		results.loc[num_file]=pd.Series(cur_row)
 	results.to_csv('go_stats_table.txt',sep="\t",mode='w')
