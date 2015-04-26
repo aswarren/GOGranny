878e8d75d1ac122e0a9cbd7bba4616fc2de8e257
Andrew Warren <anwarren@vbi.vt.edu>
2015-04-25 21:44:08 -0400

    calculating tree value

--- examples/makeEntropyChart.py
+++ examples/makeEntropyChart.py
@@ -3,30 +3,55 @@
 from GOGranny import *
 from GOGranny.GONode import GONode as GN
 from math import log
+from math import ceil
 import networkx as nx
 import matplotlib.pyplot as plt
 import itertools
+from operator import xor
+import ipdb
 
-
-def create_tree(r, h):
-	result_graph = semGODiGraph(None, Aspects.BP)
+def create_tree(r, h=None, num_nodes=None):
+    #need to have either height or num_nodes
+    assert xor(bool(h),bool(num_nodes))
+    to_remove=0
+    if num_nodes != None:
+        if r==1:
+            h=num_nodes
+        else:
+            h=ceil(log(num_nodes*(r-1)+1, r)-1)
+            init_size=(r**(h+1)-1)/(r-1)
+            to_remove=int(init_size-num_nodes)
+        
+    #branching factor of 1 does not seem to work for nx.balanced_tree
+    result_graph = semGODiGraph(None, Aspects.BP)
+    if r ==1:
+        for u in range(0,h):
+            v=u+1
+            result_graph.add_edge(GN(nodetype=GN.TERM_TYPE,dbid=v),GN(nodetype=GN.TERM_TYPE,dbid=u))
+    else:	
         internal_graph=nx.balanced_tree(r=r,h=h,create_using=nx.DiGraph()) #gnp_random_graph(10,0.5,directed=True)
+        current=internal_graph.number_of_nodes()
+        remove_nodes=range(current-to_remove,current)
+        for r in remove_nodes:
+            internal_graph.remove_node(r)
+        if num_nodes != None:
+            assert num_nodes == internal_graph.number_of_nodes()
         for u,v in internal_graph.edges_iter():
-                result_graph.add_edge(GN(nodetype=GN.TERM_TYPE,dbid=u),GN(nodetype=GN.TERM_TYPE,dbid=v))
+            result_graph.add_edge(GN(nodetype=GN.TERM_TYPE,dbid=u),GN(nodetype=GN.TERM_TYPE,dbid=v))
         nx.reverse(result_graph, copy=False) #make the edges point up not down
         root_list=[n for n,d in result_graph.out_degree().items() if d==0]
         result_graph.root=root_list[0]
-	result_graph.semMakeGraph()
-        return result_graph
-
+    result_graph.semMakeGraph()
+    return result_graph
+    
 def plotTreeBehavior():
-    marker = itertools.cycle(('+', '.', 'o', '*', ','))
+    marker = itertools.cycle(('+', '.', '*'))
     color = itertools.cycle(('r', 'g', 'b', 'c', 'm', 'y', 'k'))
     lines={}
     for h in range(2,5):#height
         x_values=[]
         y_values=[]
-        for r in range(2,10):#branching factor
+        for r in range(1,10):#branching factor
             cur_graph=create_tree(r,h)
             max_entropy=-2*log(1/float(cur_graph.num_nodes),2)
             min_entropy=-1*log(1/float(cur_graph.num_nodes),2)
@@ -34,9 +59,9 @@ def plotTreeBehavior():
             cur_index=(cur_entropy-min_entropy)/(max_entropy-min_entropy)
             x_values.append(r)
             y_values.append(cur_index)
-        lines["height:"+str(h)]=plt.plot(x_values, y_values, str(color.next())+str(marker.next()),label="height "+str(h))
+        lines["height:"+str(h)]=plt.plot(x_values, y_values, color=str(color.next()), marker=str(marker.next()),label="height "+str(h), linestyle="-")
     plt.legend(loc=4)
-    plt.axis([1,11,.4,1.1])
+    plt.axis([0,11,-0.1,1.1])
     plt.title('Branching factor vs. TI for balanced trees')
     plt.ylabel('Term-Independence')
     plt.xlabel('Branching factor')
@@ -44,4 +69,4 @@ def plotTreeBehavior():
 
 if __name__ == '__main__':
     plotTreeBehavior()
-	
+
--- examples/makeEntropyStats.py
+++ examples/makeEntropyStats.py
@@ -5,7 +5,7 @@ import networkx as nx
 import pandas as pd
 from math import log
 import os, sys
-
+import makeEntropyChart
 
 #Calculate entropy of the ontology and create and image as well
 def main(init_args):
@@ -16,7 +16,8 @@ def main(init_args):
 	
 	onto_cat=init_args[0]
 	go_files=init_args[1:]
-	results = pd.DataFrame(columns=['date','entropy','TI','edges','nodes','in_degree','out_degree','degree'])
+    tree_var=["tree"+str(i) for i in range(2,11)]
+	results = pd.DataFrame(columns=['date','entropy','TI','edges','nodes','in_degree','out_degree','degree']+tree_var)
 	num_file=0
 	for go_file in go_files:
 		num_file+=1
@@ -43,7 +44,12 @@ def main(init_args):
 		min_uncert=-log(float(1)/cur_graph.num_nodes,2)
 		max_uncert=min_uncert*2
 		ti=(graph_uncert-min_uncert)/(max_uncert-min_uncert)#for clarity. could just be min_uncert.
+        tree_values={}
+        for i in range(2,11):
+            cur_tree= makeEntropyChart.create_tree(i, h=None, num_nodes=cur_graph.num_nodes)
+            tree_values["tree"+str(i)]=graph_uncert/cur_tree.calc_graph_uncert()
 		cur_row={'date':date,'TI':ti,'entropy':graph_uncert,'edges':cur_graph.number_of_edges(),'nodes':cur_graph.num_nodes,'in_degree':float(sum(out_degree.values()))/len(degree_total),'out_degree':float(sum(in_degree.values()))/len(degree_total),'degree':float(sum(degree_total))/len(degree_total)}
+        cur_row.update(tree_values)
 		results.loc[num_file]=pd.Series(cur_row)
 	results.to_csv('go_stats_table.txt',sep="\t",mode='w')
 			
